837c81d936f32c1d2b31fd6d7c2bfa30e10daf3e
A-Ubaldo <143668619+A-Ubaldo@users.noreply.github.com>
2026-03-16 10:50:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Portfolio Loaded");
+// Dark mode
+
+document.getElementById("themeToggle")
+.addEventListener("click",function(){
+
+document.body.classList.toggle("dark-mode")
+
+})
+
+
+// Scroll animations
+
+const faders = document.querySelectorAll(".fade")
+
+const appearOptions = {
+threshold:0.3
+}
+
+const appearOnScroll =
+new IntersectionObserver(function(entries,observer){
+
+entries.forEach(entry => {
+
+if(!entry.isIntersecting){
+return
+}
+
+entry.target.classList.add("show")
+observer.unobserve(entry.target)
+
+})
+
+},appearOptions)
+
+faders.forEach(fader => {
+appearOnScroll.observe(fader)
+})
